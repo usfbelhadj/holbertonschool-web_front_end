9403fff7c07060b1e1c13707cbaf7fe2e2df485b
usfbelhadj <usf.belhadj@gmail.com>
2021-02-15 22:53:30 +0100

    Closure

--- 0x0C-Javascript_advanced/2-function_me.js
+++ 0x0C-Javascript_advanced/2-function_me.js
@@ -1,3 +1,10 @@
-function welcomeMessage() {
-
+function welcomeMessage(fullName) {
+    function closure() {
+        alert(`Welcome ${fullName}`);
+    }
+    return closure;
 }
+
+var guillaume = welcomeMessage('Guillaume');
+var alex = welcomeMessage('Alex');
+var frd = welcomeMessage('Fred');
