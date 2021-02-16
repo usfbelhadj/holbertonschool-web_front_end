d0dfb81c5be339011bb8737e03633754182a15de
usfbelhadj <usf.belhadj@gmail.com>
2021-02-16 16:03:00 +0100

    Stack order in functions Fix style

--- 0x0C-Javascript_advanced/8-payments.js
+++ 0x0C-Javascript_advanced/8-payments.js
@@ -1,10 +1,10 @@
-function processPayment(amount) {
-    console.log(`Collecting payment of ${amount}`);
+function processPayment (amount) {
+  console.log(`Collecting payment of ${amount}`);
 }
-function processOrder(orderId, amount) {
-    console.log(`${orderId} is being processed`);
-    processPayment(amount);
-    console.log(`${orderId} has been fully processed`);
+function processOrder (orderId, amount) {
+  console.log(`${orderId} is being processed`);
+  processPayment(amount);
+  console.log(`${orderId} has been fully processed`);
 }
 
 console.log('Processing orders');
